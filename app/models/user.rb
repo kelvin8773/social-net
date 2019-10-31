110b45c0a1e12c8626e7b688321f0cb33dd49983
Kelvin Liang <kelvin9877@gmail.com>
2019-10-31 21:54:28 +0800

    update facebook login default password

--- app/models/user.rb
+++ app/models/user.rb
@@ -69,7 +69,8 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
+      # user.password = Devise.friendly_token[0, 20]
+      user.password = "foobar"
       user.name = auth.info.name # assuming the user model has a name
       user.image = auth.info.image # assuming the user model has an image
     end
